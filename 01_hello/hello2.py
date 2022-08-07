dee7858bf0755647d31b9e148ddc760a2993f58f
Henry Nguyen <14987689+henryxn@users.noreply.github.com>
2022-08-06 19:16:40 -0700

    fixed minor bug, working now.

--- 01_hello/hello2.py
+++ 01_hello/hello2.py
@@ -17,9 +17,9 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument('positional', metavar='str', help='A positional argument')
-    parser.add_argument('-a', '--arg', help='A named string argument', metavar='str', type='str', default='')
-    parser.add_argument('-i', '--int', help='A named integer argument', metavar='int', type='int', default=0)
-    parser.add_argument('f', '--file', help='A readable file', type=argparse.FileType('r'), default=None)
+    parser.add_argument('-a', '--arg', help='A named string argument', metavar='str', type=str, default='')
+    parser.add_argument('-i', '--int', help='A named integer argument', metavar='int', type=int, default=0)
+    parser.add_argument('-f', '--file', help='A readable file', metavar='FILE', type=argparse.FileType('r'), default=None)
     parser.add_argument('-o', '--on', help='A boolean flag', action='store_true')
     return parser.parse_args()
 
